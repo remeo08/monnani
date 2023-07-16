78a037aad221211e9d8e933fe00ab3d4c90f6c7a
byungyong <quddyddlrk@naver.com>
2023-07-17 03:22:49 +0900

    monnani back

--- result/models.py
+++ result/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Result(models.Model):
-    result = models.CharField(max_length=5)
-    mbti = models.CharField(max_length=15)
+    result = models.CharField(max_length=30)
+    mbti = models.TextField()
     answer = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
