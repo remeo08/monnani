9f826cf9d2fdc10f3822248c1f7b0cd7a710113e
byungyong <quddyddlrk@naver.com>
2023-07-17 04:39:38 +0900

    vegi4

--- config/settings.py
+++ config/settings.py
@@ -104,8 +104,6 @@ if DEBUG:
 else:
     DATABASES = {
     'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        # default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
