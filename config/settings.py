a6468975ed2245ae979a85930dc080fb016ecb5a
byungyong <quddyddlrk@naver.com>
2023-07-17 04:47:56 +0900

    vegi5

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = "django-insecure-6i121&z34l_$ed-f^)$so@b5n#*i!r$or_$--jshss16)753gv"
+SECRET_KEY = os.environ.get('SECRET_KEY', default="django-insecure-6i121&z34l_$ed-f^)$so@b5n#*i!r$or_$--jshss16)753gv")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -57,10 +56,10 @@ SYSTEM_APPS = [
 INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
