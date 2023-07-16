34c09243a91468169d669fcc88ca20a5336f6c42
byungyong <quddyddlrk@naver.com>
2023-07-17 06:46:18 +0900

    db set

--- config/settings.py
+++ config/settings.py
@@ -109,20 +109,20 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+# if DEBUG:
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/monnani',
-            conn_max_age=600,
-        )  # how long connection lasts
-    }
+}
+# else:
+#     DATABASES = {
+#         "default": dj_database_url.config(
+#             default='postgresql://postgres:postgres@localhost:5432/monnani/',
+#             conn_max_age=600,
+#         )  # how long connection lasts
+#     }
 
 
 # Password validation
